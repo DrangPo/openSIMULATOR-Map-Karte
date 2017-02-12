a0bdc8fa79b8ba164b0ff5bec4efcf0276ff6972
ManfredAabye <openmanniland@gmx.de>
2017-02-12 14:17:34 +0100

    001

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>
+
+
+   
+   <!-- iframe hier mit kann der RegionsgeneratorV1_xx in eine bestehende Homepage eingefügt werden.
+   
+        Zusätzlich kann das ganze noch Skaliert werden damit es gut passt. Dies kann man einstellen in dem man 0.90 verändert (0.50 kleiner, 0.90 größer).
+		Beispiel: Einfach suchen ersetzen auswählen und werte eingeben 0.90 nach 0.50
+		
+        Die Farben stellt man einfach in der Hauptdatei RegionsgeneratorV1_xx.php ein. 	
+        w3-blue-grey und w3-text-red brauchen hierfür nur durch suchen und ersetzen geändert werden, siehe hier: http://www.w3schools.com/w3css/w3css_colors.asp
+	-->
+	
+<!-- Anfang -->
+<iframe 
+frameborder="1" src="./map.php" width="100%" height="100%" style="padding:1px; border:2px solid #000000;
+position:absolute; top:10px; left:10px; zoom: 0.90; -moz-transform: scale(0.90); -moz-transform-origin: 0 0; -o-transform: scale(0.90); -o-transform-origin: 0 0; -webkit-transform: scale(0.90); -webkit-transform-origin: 0 0;">
+</iframe>
+<!-- Ende -->
+
+</body></html>
